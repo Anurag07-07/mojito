c29e3ac8e349bc0537db0a8e5931ca399b6c42a3
Anurag07raj <anurag09raj@gmail.com>
2025-06-28 23:10:53 +0530

    navbar completed

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Hero(props) {
+    return (
+        <>
+        <section id={`hero`} className={` noisy`}>
+            <h1 className={`title`}>MOJITO</h1>
+            <img src="/images/hero-left-leaf.png" alt="left-leaf" className={`left-leaf`}/>
+            <img src="/images/hero-right-leaf.png" alt="right-leaf" className={`right-leaf`}/>
+            <div className={` body`}>
+                <div className={'content'}>
+                    <div className={` `}></div>
+                </div>
+            </div>
+        </section>
+        </>
+    );
+}
+
+export default Hero;
