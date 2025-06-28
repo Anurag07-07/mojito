1800d4adf072ebe7f9fe61cb38bc60a587ebe190
Anurag07raj <anurag09raj@gmail.com>
2025-06-28 21:42:48 +0530

    Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SplitText} from "gsap/SplitText";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
+
+function App() {
+    return (
+        <div className={` text-6xl bg-red-950 text-white`}>
+
+        </div>
+    );
+}
+
+export default App;
